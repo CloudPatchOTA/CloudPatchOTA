091bd7676326363543101a40dfed627e76c2c5b9
kiranthomas77 <97358437+kiranthomas77@users.noreply.github.com>
2023-06-05 17:36:43 +0530

    Update OTAUpdateModule.c

--- lib/OTAUpdateModule.c
+++ lib/OTAUpdateModule.c
@@ -1,66 +1,203 @@
+
+
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <HTTPClient.h>
 #include <Update.h>
 
-// Network configuration variables
-const char* ssid = "Pixel";
-const char* password = "12345678";
-const char* host = "http://3.110.114.64/“;
-const int port = 443;
+WiFiClient client;
 
-// Update file variables
-const char* updateUrl = "http://13.233.137.149/updated.bin”;
-const char* updateFilename = "updated.bin";
 
-// Task handle for Communication module task
-TaskHandle_t CommunicationTaskHandle = NULL;
+long contentLength = 0;
+bool isValidContentType = false;
 
-// Communication module task
-void CommunicationTask(void *pvParameters) {
-  // Connect to WiFi network
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+const char* SSID = "YOUR-SSID";
+const char* PSWD = "YOUR-SSID-PSWD";
+
+// S3 Bucket Config
+
+//String host = "bucket-name.s3.ap-south-1.amazonaws.com"; // Host => bucket-name.s3.region.amazonaws.com
+String host = "https://bucketformyfrontendapplication.s3.ap-south-1.amazonaws.com/";
+
+int port = 80; // Non https. For HTTPS 443. As of today, HTTPS doesn't work.
+
+//String bin = "/sketch-name.ino.bin"; // bin file name with a slash in front.
+String bin = "/updated.bin";
+
+// Utility to extract header value from headers
+String getHeaderValue(String header, String headerName) {
+  return header.substring(strlen(headerName.c_str()));
+}
+
+// OTA Logic 
+void execOTA() {
+  Serial.println("Connecting to: " + String(host));
+  // Connect to S3
+  if (client.connect(host.c_str(), port)) {
+    // Connection Succeed.
+    // Fecthing the bin
+    Serial.println("Fetching Bin: " + String(bin));
+
+    // Get the contents of the bin file
+    client.print(String("GET ") + bin + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Cache-Control: no-cache\r\n" +
+                 "Connection: close\r\n\r\n");
+
+    // Check what is being sent
+    //    Serial.print(String("GET ") + bin + " HTTP/1.1\r\n" +
+    //                 "Host: " + host + "\r\n" +
+    //                 "Cache-Control: no-cache\r\n" +
+    //                 "Connection: close\r\n\r\n");
+
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+      if (millis() - timeout > 5000) {
+        Serial.println("Client Timeout !");
+        client.stop();
+        return;
+      }
+    }
+    // Once the response is available,
+    // check stuff
+
+    /*
+       Response Structure
+        HTTP/1.1 200 OK
+        x-amz-id-2: NVKxnU1aIQMmpGKhSwpCBh8y2JPbak18QLIfE+OiUDOos+7UftZKjtCFqrwsGOZRN5Zee0jpTd0=
+        x-amz-request-id: 2D56B47560B764EC
+        Date: Wed, 14 Jun 2017 03:33:59 GMT
+        Last-Modified: Fri, 02 Jun 2017 14:50:11 GMT
+        ETag: "d2afebbaaebc38cd669ce36727152af9"
+        Accept-Ranges: bytes
+        Content-Type: application/octet-stream
+        Content-Length: 357280
+        Server: AmazonS3
+                                   
+        {{BIN FILE CONTENTS}}
+
+    */
+    while (client.available()) {
+      // read line till /n
+      String line = client.readStringUntil('\n');
+      // remove space, to check if the line is end of headers
+      line.trim();
+
+      // if the the line is empty,
+      // this is end of headers
+      // break the while and feed the
+      // remaining `client` to the
+      // Update.writeStream();
+      if (!line.length()) {
+        //headers ended
+        break; // and get the OTA started
+      }
+
+      // Check if the HTTP Response is 200
+      // else break and Exit Update
+      if (line.startsWith("HTTP/1.1")) {
+        if (line.indexOf("200") < 0) {
+          Serial.println("Got a non 200 status code from server. Exiting OTA Update.");
+          break;
+        }
+      }
+
+      // extract headers here
+      // Start with content length
+      if (line.startsWith("Content-Length: ")) {
+        contentLength = atol((getHeaderValue(line, "Content-Length: ")).c_str());
+        Serial.println("Got " + String(contentLength) + " bytes from server");
+      }
+
+      // Next, the content type
+      if (line.startsWith("Content-Type: ")) {
+        String contentType = getHeaderValue(line, "Content-Type: ");
+        Serial.println("Got " + contentType + " payload.");
+        if (contentType == "application/octet-stream") {
+          isValidContentType = true;
+        }
+      }
+    }
+  } else {
+    // Connect to S3 failed
+    // May be try?
+    // Probably a choppy network?
+    Serial.println("Connection to " + String(host) + " failed. Please check your setup");
+    // retry??
+    // execOTA();
   }
 
-  // Establish secure connection to update portal
-  WiFiClientSecure client;
-  if (!client.connect(host, port)) {
-    Serial.println("Failed to connect to update portal");
-    vTaskDelete(CommunicationTaskHandle);
-    return;
-  }
+  // Check what is the contentLength and if content type is `application/octet-stream`
+  Serial.println("contentLength : " + String(contentLength) + ", isValidContentType : " + String(isValidContentType));
 
-  // Request update file
-  HTTPClient http;
-  http.begin(client, updateUrl);
-  int httpCode = http.GET();
-  if (httpCode != HTTP_CODE_OK) {
-    Serial.println("Failed to download update file");
-    vTaskDelete(CommunicationTaskHandle);
-    return;
-  }
+  // check contentLength and content type
+  if (contentLength && isValidContentType) {
+    // Check if there is enough to OTA Update
+    bool canBegin = Update.begin(contentLength);
 
-  // Save update file to SPIFFS
-  File updateFile = SPIFFS.open(updateFilename, FILE_WRITE);
-  if (!updateFile) {
-    Serial.println("Failed to open update file");
-    vTaskDelete(CommunicationTaskHandle);
-    return;
-  }
-  http.writeToStream(&updateFile);
-  updateFile.close();
+    // If yes, begin
+    if (canBegin) {
+      Serial.println("Begin OTA. This may take 2 - 5 mins to complete. Things might be quite for a while.. Patience!");
+      // No activity would appear on the Serial monitor
+      // So be patient. This may take 2 - 5mins to complete
+      size_t written = Update.writeStream(client);
 
-void setup()
- {
+      if (written == contentLength) {
+        Serial.println("Written : " + String(written) + " successfully");
+      } else {
+        Serial.println("Written only : " + String(written) + "/" + String(contentLength) + ". Retry?" );
+        // retry??
+        // execOTA();
+      }
+
+      if (Update.end()) {
+        Serial.println("OTA done!");
+        if (Update.isFinished()) {
+          Serial.println("Update successfully completed. Rebooting.");
+          ESP.restart();
+        } else {
+          Serial.println("Update not finished? Something went wrong!");
+        }
+      } else {
+        Serial.println("Error Occurred. Error #: " + String(Update.getError()));
+      }
+    } else {
+      // not enough space to begin OTA
+      // Understand the partitions and
+      // space availability
+      Serial.println("Not enough space to begin OTA");
+      client.flush();
+    }
+  } else {
+    Serial.println("There was no content in the response");
+    client.flush();
+  }
+}
+
+void setup() {
+  //Begin Serial
   Serial.begin(115200);
-  SPIFFS.begin();
+  delay(10);
 
-  // Create Communication module task
-  xTaskCreatePinnedToCore(CommunicationTask, "Communication Task", 4096, NULL, 1, &CommunicationTaskHandle, 0);
+  Serial.println("Connecting to " + String(SSID));
+
+  // Connect to provided SSID and PSWD
+  WiFi.begin(SSID, PSWD);
+
+  // Wait for connection to establish
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("."); // Keep the serial monitor lit!
+    delay(500);
+  }
+
+  // Connection Succeed
+  Serial.println("");
+  Serial.println("Connected to " + String(SSID));
+
+  // Execute OTA Update
+  execOTA();
 }
 
 void loop() {
-  // do nothing
+  
 }
+
