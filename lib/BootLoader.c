e4dd6933cbb40563ee803d5698435d454f2804b3
kiranthomas77 <97358437+kiranthomas77@users.noreply.github.com>
2023-06-05 17:33:19 +0530

    Update BootLoader.c

--- lib/BootLoader.c
+++ lib/BootLoader.c
@@ -1,8 +1,49 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Update.h>
 
-void UpdateService(int FIRMWARE_VERVION)
-{
-    if(CONNECTED)
-    {
-        
-    }
+const char* ssid = "Pixel";
+const char* password = "12345678";
+
+
+//  server should return "no update" if no update available, or the binary file if an update is available
+const char* update_server = "http://www.test.com/firmware";
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
 }
+
+void loop() {
+  delay(1000 * 60 * 60); // Wait for an hour between checks, you can adjust this delay as needed
+  checkForUpdates();
+}
+
+void checkForUpdates() {
+  HTTPClient http;
+
+  http.begin(update_server); // Specify the URL
+  int httpCode = http.GET(); // Make the request
+
+  if (httpCode == 200) { // Check the returning code
+    String payload = http.getString(); // Get the request response payload
+    if (payload == "no update") {
+      Serial.println("No update available.");
+    } else {
+      Serial.println("Update available. Updating...");
+      // Run the OTA update
+      execOTA(); // Insert the OTA update code here
+    }
+  } else {
+    Serial.println("Error on HTTP request");
+  }
+
+  http.end(); // Free the resources
+}
+
