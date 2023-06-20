53de499ab5ab313a543a42690e1b58c3b71adb67
PatriotArrow <40357308+PatriotArrow@users.noreply.github.com>
2023-06-20 16:34:11 +0530

    Added new functions to test

--- client/test_sketches/test8_main_header/main/main.ino
+++ client/test_sketches/test8_main_header/main/main.ino
@@ -4,15 +4,37 @@
 //#include "C:\Users\kiran\OneDrive\Documents\GitHub\CloudPatchOTA\lib\secrets.h"
 
 float firmware = 1.0;
+#define ledPin 2
+
+void blinkFunction()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(600); 
+  for (int i = 0 ; i < 5 ; i++)
+  {
+    digitalWrite(ledPin, LOW);
+    delay(100);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+  }  
+}
+
+void printFunction()
+{
+  Serial.print("Firmware : ");
+  Serial.println(firmware);
+}
+
 void setup()
 {
   Serial.begin(115200);
   setupOTA(3000,3000);
+  pinMode(ledPin, OUTPUT);
 }
 void loop()
-{
-  
+{  
   checkMQTTIncomingMsg();
-  Serial.print("Firmware : ");
-  Serial.println(firmware);
+  blinkFunction();
+  printFunction();
+
 }
